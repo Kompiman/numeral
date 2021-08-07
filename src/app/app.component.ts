24d07b3169c22edc913db79cb6738f5a49073b49
Rizky <rizky.maulana@anteraja.id>
2021-08-07 10:56:31 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { startWith, map, debounceTime, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'numeral-test';
+
+  public hasil1: any
+  hasil2
+  v1
+  v2
+  ngOnInit() {
+    // this.onType()
+  }
+
+  count(n) {
+    let result = 1;
+    for (let i = n; 0 < i; i--) {
+      result = result * i;
+    }
+
+    console.log(result)
+    this.v1 = result
+  }
+
+  strain(n) {
+    let jumlahTerbanyak = 0;
+    let charTerbanyak = "";
+    for (let i = 0; i < n.length; i++) {
+      let stringCheck = n.charAt(i);
+      let jumlahChar = 0;
+      for (let j = 0; j < n.length; j++) {
+        if (stringCheck === n.charAt(j)) {
+          jumlahChar++;
+        }
+      }
+      if (jumlahTerbanyak < jumlahChar) {
+        charTerbanyak = stringCheck;
+        jumlahTerbanyak = jumlahChar
+      }
+    }
+
+    console.log(charTerbanyak)
+    this.v2 = charTerbanyak
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSortModule } from '@angular/material/sort';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import {ClipboardModule} from '@angular/cdk/clipboard';
+import { HttpClientModule } from '@angular/common/http';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatTabsModule} from '@angular/material/tabs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +34,32 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NgbModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatNativeDateModule,
+    MatExpansionModule,
+    MatStepperModule,
+    MatSnackBarModule,
+    MatIconModule,
+    MatCardModule,
+    MatInputModule,
+    MatDividerModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    ReactiveFormsModule,
+    MatPaginatorModule,
+    FormsModule,
+    RouterModule,
+    HttpClientModule,
+    MatAutocompleteModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    ClipboardModule,
+    MatRadioModule,
+    MatTabsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
